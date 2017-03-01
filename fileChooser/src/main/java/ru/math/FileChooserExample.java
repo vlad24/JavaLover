7b376dabac7af162dd0d3c9ab1145cca9cbf40c0
Vladislav Pavlov <vlad.pavlov24@gmail.com>
2017-03-01 21:24:10 +0300

    file chooser demo added

---
+++ fileChooser/src/main/java/ru/math/FileChooserExample.java
@@ -0,0 +1,59 @@
+package ru.math;
+
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class FileChooserExample extends JFrame {
+
+	public FileChooserExample() {
+		super("Chooser");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JPanel panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		panel.add(Box.createVerticalGlue());
+		final JLabel label = new JLabel("Chosen file");
+		label.setAlignmentX(CENTER_ALIGNMENT);
+		panel.add(label);
+		panel.add(Box.createRigidArea(new Dimension(10, 10)));
+		JButton button = new JButton("Show chooser");
+		button.setAlignmentX(CENTER_ALIGNMENT);
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser fileopen = new JFileChooser();             
+				int ret = fileopen.showDialog(null, "Open file");                
+				if (ret == JFileChooser.APPROVE_OPTION) {
+					File file = fileopen.getSelectedFile();
+					label.setText(file.getName());
+				}
+			}
+		});
+		panel.add(button);
+		panel.add(Box.createVerticalGlue());
+		getContentPane().add(panel);
+		setPreferredSize(new Dimension(260, 220));
+		pack();
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				JFrame.setDefaultLookAndFeelDecorated(true);
+				JDialog.setDefaultLookAndFeelDecorated(true);
+				new FileChooserExample();
+			}
+		});
+	}
+}	
