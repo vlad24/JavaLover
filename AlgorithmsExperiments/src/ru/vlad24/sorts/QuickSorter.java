8ee7fe767ea14e98f82eaac56a3105c414adadf4
Vladislav <vlad.pavlov24@gmail.com>
2020-06-25 18:57:05 +0200

    Update QuickSorter.java
    Format and rename

--- AlgorithmsExperiments/src/ru/vlad24/sorts/QuickSorter.java
+++ AlgorithmsExperiments/src/ru/vlad24/sorts/QuickSorter.java
@@ -3,39 +3,40 @@ package ru.vlad24.sorts;
 public class QuickSorter implements Sorter{
 
 	@Override
-	public void sort(int[] a) {
-		qsort(a, 0, a.length - 1);
-	}
+	 public void sort(int[] array) {
+        quickSort(array, 0, array.length - 1);
+    }
 
-	public void qsort(int[] a, int left, int right) {
-		if (left < right - 1){
-			int m = settleCentral(a, left, right);
-			qsort(a,  left, m);
-			qsort(a, m, right);
-		}
-	}
-	
 
-	private int settleCentral(int[] a , int left, int right) {
-		int i = left;
-		int j = right;
-		int m = (left + right) / 2;
-		while (i < j){
-			while(a[i] <= a[m] && i < m){
-				i++;
-			}
-			while(a[j] >= a[m] && j > m){
-				j--;
-			}
-			int tmp = a[i];
-			a[i] = a[j];
-			a[j] = tmp;
-			if (i == m){
-				m = j;
-			}else if (j == m){
-				m = i;
-			}			
-		}
-		return m;
-	}
+    public void quickSort(int[] array, int left, int right) {
+        if (left < right - 1) {
+            int pivot = settlePivot(array, left, right);
+            quickSort(array, left, pivot);
+            quickSort(array, pivot, right);
+        }
+    }
+
+
+    private int settlePivot(int[] array, int left, int right) {
+        int i = left;
+        int j = right;
+        int pivot = (left + right) / 2;
+        while (i < j) {
+            while (array[i] <= array[pivot] && i < pivot) {
+                i++;
+            }
+            while (array[j] >= array[pivot] && j > pivot) {
+                j--;
+            }
+            int tmp = array[i];
+            array[i] = array[j];
+            array[j] = tmp;
+            if (i == pivot) {
+                pivot = j;
+            } else if (j == pivot) {
+                pivot = i;
+            }
+        }
+        return pivot;
+    }
 }
